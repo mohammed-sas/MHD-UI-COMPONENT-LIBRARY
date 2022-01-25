c79c3ba73faa8a38934406beb9de7aced2bec04d
mohammed-sas <89216938+mohammed-sas@users.noreply.github.com>
2022-01-25 18:41:08 +0530

    feat : smooth scroll

--- docs.js
+++ docs.js
@@ -1,5 +1,7 @@
 const dropDownBtn = document.querySelector('.sidebar-btn');
 const dropDownList = document.querySelector('.sidebar-list');
+const sectionsAnchor = document.querySelectorAll('.sidebar-list span a');
+
 let btnrotate=0;
 dropDownBtn.addEventListener('click',()=>{
         dropDownList.classList.toggle('display-flex')
@@ -9,4 +11,17 @@ dropDownBtn.addEventListener('click',()=>{
 
 dropDownList.addEventListener('click',()=>{
     dropDownList.classList.toggle('display-flex');
+})
+
+sectionsAnchor.forEach(link=>{
+    link.addEventListener('click',(e)=>{
+        e.preventDefault(); 
+        let query = link.getAttribute('href');
+        console.log(query);
+        let destination = document.querySelector(query);
+        console.log(destination)
+        destination.scrollIntoView({
+            behavior:'smooth',
+        })
+})
 })
